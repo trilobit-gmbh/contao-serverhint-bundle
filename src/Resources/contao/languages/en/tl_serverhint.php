3acd747496a7c42ab3cc9cd9262b4820f9ee9b34
trilobit-gmbh <trilobit-gmbh@users.noreply.github.com>
2018-01-08 09:14:17 +0100

    Typo

--- src/Resources/contao/languages/en/tl_serverhint.php
+++ src/Resources/contao/languages/en/tl_serverhint.php
@@ -20,6 +20,6 @@ $GLOBALS['TL_LANG']['tl_settings']['serverhint'][0] = 'Server';
 $GLOBALS['TL_LANG']['tl_settings']['serverhint'][1] = 'Please add "HTTP_HOST", highlight color and the server hint message.';
 
 $GLOBALS['TL_LANG']['tl_settings']['options']['server']    = 'Server';
-$GLOBALS['TL_LANG']['tl_settings']['options']['color']     = 'Hilight color';
+$GLOBALS['TL_LANG']['tl_settings']['options']['color']     = 'Highlight color';
 $GLOBALS['TL_LANG']['tl_settings']['options']['hint']      = 'Server hint message';
 $GLOBALS['TL_LANG']['tl_settings']['options']['published'] = 'active';
