1bfa386e0deda4dfd2bf7ce719af5c1e1a857b43
Peter Adelmann <peter.adelmann@trilobit.de>
2021-06-01 12:56:50 +0200

    use be output template hook for server hint theming

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -7,54 +7,9 @@
  * @link       http://github.com/trilobit-gmbh/contao-serverhint-bundle
  */
 
-use Contao\Config;
-use Contao\StringUtil;
-use Trilobit\ServerhintBundle\ServerHintWizard;
+use Trilobit\ServerhintBundle\Widget\ServerHintWizard;
+use Trilobit\ServerhintBundle\Template\BackendTemplate;
 
-/*
- * Back end form fields
- */
 $GLOBALS['BE_FFL']['serverHintWizard'] = ServerHintWizard::class;
 
-/*
- * Add css
- */
-if (TL_MODE === 'BE') {
-    $arrServerhint = StringUtil::deserialize(Config::get('serverhint'), true);
-
-    $strStyle = '';
-
-    if (Config::get('serverhint')
-        && count($arrServerhint)
-    ) {
-        foreach ($arrServerhint as $value) {
-            if ('' === $value['published']) {
-                continue;
-            }
-
-            if (strtolower($_SERVER['HTTP_HOST']) === strtolower($value['value'])) {
-                $value['label'] = str_replace('&#35;', '#', $value['label']);
-                $value['label'] = str_replace('&#40;', '(', $value['label']);
-                $value['label'] = str_replace('&#41;', ')', $value['label']);
-
-                $strStyle .= '#header .inner { background-color: '.$value['label'].'; } '
-                    .'#tl_navigation .menu_level_1 li.current { border-left-color: '.$value['label'].'; } '
-                    .'#tmenu a:hover, '
-                    .'#tmenu a.hover, '
-                    .'#tmenu li:hover .h2, '
-                    .'#tmenu .active .h2, '
-                    .'#tmenu .burger button:hover '
-                    .'{ background-color: '.$value['label'].'; } '
-                ;
-
-                if ('' !== $value['hint']) {
-                    $strStyle .= '#home:after '
-                        .'{ content: "'.$value['hint'].'"; display: inline-block; font-family: "Architects Daughter", cursive; font-size: 1rem; color: rgb(0,16,53); transform: rotate(-1deg); width: auto; padding-left: 15px; }';
-                }
-            }
-        }
-
-        $GLOBALS['TL_HEAD'][] = '<style>'.$strStyle.'</style>';
-        $GLOBALS['TL_MOOTOOLS'][] = '<style>'.$strStyle.'</style>';
-    }
-}
+$GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = [BackendTemplate::class, '__invoke'];
