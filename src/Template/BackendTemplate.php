f024ca145e03211473435376d6014865d66d48a0
Peter Adelmann <peter.adelmann@trilobit.de>
2021-06-01 15:20:28 +0200

    apply c44 support / fix and improve

--- src/Template/BackendTemplate.php
+++ src/Template/BackendTemplate.php
@@ -16,6 +16,8 @@ class BackendTemplate
 {
     public function __invoke(string $buffer): string
     {
+        $adjustPercent = .15;
+
         $hint = StringUtil::deserialize(Config::get('serverhint'), true);
 
         $style = '';
@@ -31,17 +33,19 @@ class BackendTemplate
                     $value['label'] = str_replace('&#40;', '(', $value['label']);
                     $value['label'] = str_replace('&#41;', ')', $value['label']);
 
-                    $style .= '#header .inner { background-color: '.$value['label'].'; } '
-                        .'#tl_navigation .menu_level_1 li.current { border-left-color: '.$value['label'].'; } '
-                        .'#tmenu a:hover, '
-                        .'#tmenu a.hover, '
-                        .'#tmenu li:hover .h2, '
-                        .'#tmenu .active .h2, '
-                        .'#tmenu .burger button:hover '
+                    $hoverColor = $this->adjustBrightness($value['label'], $adjustPercent);
+
+                    $style .= '#header .inner '
                         .'{ background-color: '.$value['label'].'; } '
+                        .'#tl_navigation .tl_level_2 li.active '
+                        .'{ border-left-color: '.$value['label'].'; } '
+                        .'#tl_navigation .menu_level_1 li.current '
+                        .'{ border-left-color: '.$value['label'].'; } '
+                        .'#tmenu a:hover, #tmenu a.hover, #tmenu li:hover .h2, #tmenu .active .h2, #tmenu .burger button:hover { background-color: '.$hoverColor.'; } '
+                        .'#tmenu a:hover, #tmenu li:hover h2, #tmenu .burger button:hover { background-color: '.$hoverColor.'; } '
                     ;
 
-                    if ('' !== $value['hint']) {
+                    if (!empty($value['hint'])) {
                         $style .= '#home:after '
                             .'{ content: "'.$value['hint'].'"; display: inline-block; font-family: "Architects Daughter", cursive; font-size: 1rem; color: rgb(0,16,53); transform: rotate(-1deg); width: auto; padding-left: 15px; }';
                     }
@@ -49,10 +53,30 @@ class BackendTemplate
             }
             if (!empty($style)) {
                 $style = '<style>'.$style.'</style>';
-                $buffer = \preg_replace('/<body/', $style.'<body', $buffer);
+                $buffer = preg_replace('/<body/', $style.'<body', $buffer);
             }
         }
 
         return $buffer;
+    }
+
+    public function adjustBrightness($hexCode, $adjustPercent)
+    {
+        $hexCode = ltrim($hexCode, '#');
+
+        if (3 === \strlen($hexCode)) {
+            $hexCode = $hexCode[0].$hexCode[0].$hexCode[1].$hexCode[1].$hexCode[2].$hexCode[2];
+        }
+
+        $hexCode = array_map('hexdec', str_split($hexCode, 2));
+
+        foreach ($hexCode as &$color) {
+            $adjustableLimit = $adjustPercent < 0 ? $color : 255 - $color;
+            $adjustAmount = ceil($adjustableLimit * $adjustPercent);
+
+            $color = str_pad(dechex($color + $adjustAmount), 2, '0', \STR_PAD_LEFT);
+        }
+
+        return '#'.implode('', $hexCode);
     }
 }
